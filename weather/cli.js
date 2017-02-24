71e7cf560d6835ac308bce73d999613f1a054e0b
hope <lounesfeniri@gmail.com>
2017-02-24 19:08:03 +0100

    CLI Projet

--- weather/cli.js
+++ weather/cli.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 'use strict';
 
-const meow = require('meow');
+const meow = require('meow'); // 
 const chalk = require('chalk');
 const updateNotifier = require('update-notifier');
 const pkg = require('./package.json');
 const weather = require('./');
 
+// Object to help and give example for Users
 const cli = meow({
 	help: [
 		'Usage',
@@ -25,21 +26,27 @@ const cli = meow({
 	]
 });
 
+/**
+*convert temperature to celcieus 
+*@param {double} temp - the tempetarure 
+*/
 function _toCelcius(temp) {
 	return Math.round(((temp - 32) * 5) / 9);
 }
 
 updateNotifier({ pkg}).notify();
-
+/** 
+*build right commande to send it to index.js in order to start application with rigth arguments 
+*/
 weather(cli.input, (err, result) => {
 	if (err) {
 		console.log(chalk.bold.red(err));
 		process.exit(1);
 	}
-
+	
 	let condition = result.query.results.channel.item.condition.text;
 	let temperature;
-
+	
 	if (cli.input[2] && cli.input[2] === 'C') {
 		temperature = _toCelcius(result.query.results.channel.item.condition.temp) + 'C';
 	} else if (cli.input[2] && cli.input[2] === 'F') {
--- weather/index.js
+++ weather/index.js
@@ -1,16 +1,18 @@
 'use strict';
-
+/* import YQL module to send query request and get data response from yahoo API */
 const YQL = require('yql');
 const _ = require('lodash');
+
 
 module.exports = (opts, callback) => {
 	opts = opts || [];
 
 	let query;
-
+	/* if there is no arguments (city & country), we use the default arguments (Dhaka, Bangladesh) */
 	if (_.isEmpty(opts)) {
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Dhaka, Bangladesh")');
 	} else {
+	/* else send query request with user arguments */ 
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + opts[0] + ', ' + opts[1] + '")');
 	}
 
